2fd538aa5a7f5d3aa08c7366d0967a576fdbeaac
Katlego-Sekoele <sekoelekatlego@gmail.com>
2024-07-02 08:26:46 +0200

    chore: Update CSS styles for mobile responsiveness

--- src/controllers/index-controller.js
+++ src/controllers/index-controller.js
@@ -18,7 +18,17 @@ const getJoinGamePage = async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/player/join-game.html"));
 };
 
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const getCreateGame = async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/host/create-game.html"));
+};
+
+
 module.exports = {
   getHomePage,
-  getJoinGamePage
+  getJoinGamePage,
+  getCreateGame
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-const { getHomePage, getJoinGamePage } = require("../controllers/index-controller");
+const { getHomePage, getJoinGamePage, getCreateGame } = require("../controllers/index-controller");
 const indexRouter = require("express").Router();
 
 indexRouter.get("/", getHomePage);
 indexRouter.get("/player/join-game", getJoinGamePage)
+
+indexRouter.get("/host/create-game", getCreateGame)
 
 module.exports = {
   indexRouter,
