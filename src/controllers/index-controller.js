edfeb42b9f7f08b96d716d2242eec336f496a46c
andre <andreludovicusburger@gmail.com>
2024-07-03 13:36:38 +0200

    fix index router

--- src/controllers/index-controller.js
+++ src/controllers/index-controller.js
@@ -21,82 +21,83 @@ const getHomePage = async (req, res) => {
  */
 const getGamePage = async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/player/game.html"));
-  const createSession = async (req, res) => {
-    const sessionId = generateUID();
-    sessions.set(sessionId, {
-      hostUsername: req.data?.tokenData?.username,
-      status: "lobby",
-      players: [],
-      round: 0,
-    });
-    res.redirect(`/lobby/${sessionId}`);
-  };
+};
+const createSession = async (req, res) => {
+  const sessionId = generateUID();
+  sessions.set(sessionId, {
+    hostUsername: req.data?.tokenData?.username,
+    status: "lobby",
+    players: [],
+    round: 0,
+  });
+  res.redirect(`/lobby/${sessionId}`);
+};
 
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  const joinSession = async (req, res) => {
-    const { sessionId } = req.params;
-    if (sessions.get(sessionId)) {
-      const isHost =
-        sessions.get(sessionId).hostUsername === req.data?.tokenData?.username;
-      res.sendFile(
-        path.join(__dirname, `../views/${isHost ? "host" : "player"}/lobby.html`)
-      );
-    } else {
-      res.status(404).send("Lobby not found");
-    }
-  };
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const joinSession = async (req, res) => {
+  const { sessionId } = req.params;
+  if (sessions.get(sessionId)) {
+    const isHost =
+      sessions.get(sessionId).hostUsername === req.data?.tokenData?.username;
+    res.sendFile(
+      path.join(__dirname, `../views/${isHost ? "host" : "player"}/lobby.html`)
+    );
+  } else {
+    res.status(404).send("Lobby not found");
+  }
+};
 
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  const getJoinPage = async (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/join.html"));
-  };
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const getJoinPage = async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/join.html"));
+};
 
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  const getSignInPage = async (req, res) => {
-    if (req.data?.tokenData) {
-      res.redirect("/");
-    } else {
-      res.sendFile(path.join(__dirname, "../views/signin.html"));
-    }
-  };
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const getSignInPage = async (req, res) => {
+  if (req.data?.tokenData) {
+    res.redirect("/");
+  } else {
+    res.sendFile(path.join(__dirname, "../views/signin.html"));
+  }
+};
 
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  const signInUser = async (req, res) => {
-    const { username } = req.body;
-    if (!users.has(username)) {
-      const token = jwt.sign({ username }, JWT_SECRET);
-      res.cookie("token", token, { httpOnly: true });
-      res.redirect("/");
-    }
-  };
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const signInUser = async (req, res) => {
+  const { username } = req.body;
+  if (!users.has(username)) {
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/");
+  }
+};
 
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  const getSpectatorPage = async (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/spectator/index.html"));
-  };
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+const getSpectatorPage = async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/spectator/index.html"));
+};
 
-  module.exports = {
-    createSession,
-    getJoinPage,
-    getHomePage,
-    getGamePage,
-    getSpectatorPage,
-    getSignInPage,
-    signInUser,
-    joinSession,
-  };
+module.exports = {
+  createSession,
+  getJoinPage,
+  getHomePage,
+  getGamePage,
+  getSpectatorPage,
+  getSignInPage,
+  signInUser,
+  joinSession,
+};
