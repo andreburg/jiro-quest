bda1b6993855f8af23d224fe76e357172d19ac57
andre <andreludovicusburger@gmail.com>
2024-07-03 17:42:00 +0200

    fix href

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,6 +1,6 @@
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 
-export const socket = io("http://localhost:9990");
+export const socket = io("/");
 
 const sessionIdMatch = window.location.pathname.match(/\/lobby\/([^/]+)/);
 const sessionId = sessionIdMatch ? sessionIdMatch[1] : null;
