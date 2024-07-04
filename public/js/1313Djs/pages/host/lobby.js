5dc740f0c7b8fbc8b3f8b0f79d879462b0c3eb36
andre <andreludovicusburger@gmail.com>
2024-07-04 03:09:58 +0200

    working player and host

--- public/js/1313Djs/pages/host/lobby.js
+++ public/js/1313Djs/pages/host/lobby.js
@@ -1,3 +1,4 @@
+import { initializeGyroscope } from "../../../map.js";
 import { session } from "../../client/client.js";
 import Page from "../page.js";
 export default class LobbyHost extends Page {
@@ -7,94 +8,92 @@ export default class LobbyHost extends Page {
     // this.comps.Add("calculator", new Calculator());
   }
 
-  getHtml() {
-    // return `
-    //         ${this.comps.Render("nav")}
-    //         <div id="calc-page">
-    //             ${this.comps.Render("calculator")}
-    //         </div>
-    //     `;
-    return `
-    <div class="center-div">
-        <h1>Waiting For Players...</h1>
-        <table class="table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Username</th>
-                <th>Ready</th>
-            </tr>
-            </thead>
-            <tbody>
-            ${session.state.players
-              ?.map((player, i) => {
-                return `
-            <tr>
-                <td>${i + 1}</td>
-                <td>${player.username}</td>
-                <td>${player.ready}</td>
+  sideEffects() {
+    document.querySelectorAll(".kick-player-button").forEach((kickButton) => {
+      kickButton.addEventListener("click", () => {
+        kickPlayer(kickButton.id.split("-")[3]);
+      });
+    });
+    if (document.querySelector("#start-game-button"))
+      document
+        .querySelector("#start-game-button")
+        .addEventListener("click", () => {
+          session.socket.emit("sessionStateChange", {
+            session: {
+              ...session.state,
+              route: "round",
+              round: session.state.round + 1,
+            },
+          });
+        });
 
-                <td>
-                ${
-                  player.username !== session.state.hostUsername
-                    ? `
-                <div
-                    class="kick-player-button"
-                    id="kick-player-button-${player.username}"
-                >
-                    <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                    <circle cx="9" cy="9" r="8.5" fill="#FF6B6B" stroke="black" />
-                    <line
-                        x1="6"
-                        y1="9"
-                        x2="12"
-                        y2="9"
-                        stroke="black"
-                        stroke-width="2"
-                    />
-                    </svg>
-                </div>
-                `
-                    : ""
-                }
-                </td>
-            </tr>
-            `;
-              })
-              .join("")}
-            </tbody>
-        </table>
-        <div>
-            <button class="button button-danger button-medium" onclick="{}">
-            Exit
-            </button>
+    if (document.querySelector("#readyButton"))
+      initializeGyroscope(
+        session.socket,
+        document.querySelector("#readyButton")
+      );
+  }
+
+  getHtml() {
+    return `
+    <div class="center-div host-page-container">
+      <img src="/public/assets/brutalism/Brutalist 79.png" alt="random illustration" />
+      <div class="join-prompt vertical">
+        <h1>Waiting For Players...</h1>
+        <h2>${window.location.href.split("/").pop()}</h2>
+      </div>
+        <div class="lobby-player-list-container">
+                ${session.state.players?.map((player, i) => {
+                  return `
+                      <div class="lobby-player-list-item ${
+                        player.ready ? "ready" : ""
+                      }">
+                        <div class="lobby-player-list-item-vertical">
+                          <div class="lobby-player-list-item-username">
+                            ${player.username}
+                          </div>
+                          <div class="lobby-player-list-item-ready">
+                            ${player.ready ? "Ready" : "Not Ready"}
+                          </div>
+                        </div>
+                        ${
+                          player.username !== session.state.hostUsername
+                            ? `                         
+                      <div class="kick-player-button" id="kick-player-button-${player.username}">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <circle cx="9" cy="9" r="8.5" fill="#FF6B6B" stroke="black"/>
+                          <line x1="6" y1="9" x2="12" y2="9" stroke="black" stroke-width="2"/>
+                        </svg>
+                      </div>`
+                            : ""
+                        }
+                    </div>
+
+                    `;
+                })}
+        </div>
+
+        <div class="right-aligned-button-group">
             ${
               session.state.players?.find(
                 (player) => player.socketId === session.socket.id
               )?.ready
                 ? ""
                 : `
-            <button id="readyButton">Ready Up</button>
-            `
+                  <button id="readyButton">Ready Up</button>
+                  `
             }
+            ${
+              session.state.players?.every((player) => player.ready)
+                ? `
+              <div>
+                <button class="" id="start-game-button">Start</button>
+              </div>
+              `
+                : ""
+            }
+            <button class="button-danger" onclick={}>Exit</button>
         </div>
-        ${
-          session.state.players?.every((player) => player.ready)
-            ? `
-        <div>
-            <button class="button button-success button-medium" id="start-game-button">
-            Start
-            </button>
-        </div>
-        `
-            : ""
-        }
     </div>
     `;
   }
--- public/js/1313Djs/pages/player/lobby.js
+++ public/js/1313Djs/pages/player/lobby.js
@@ -1,3 +1,5 @@
+import { initializeGyroscope } from "../../../map.js";
+import { session } from "../../client/client.js";
 import Page from "../page.js";
 
 export default class LobbyPlayer extends Page {
@@ -7,17 +9,54 @@ export default class LobbyPlayer extends Page {
     // this.comps.Add("calculator", new Calculator());
   }
 
+  sideEffects() {
+    if (document.querySelector("#readyButton"))
+      initializeGyroscope(
+        session.socket,
+        document.querySelector("#readyButton")
+      );
+  }
+
   getHtml() {
-    // return `
-    //         ${this.comps.Render("nav")}
-    //         <div id="calc-page">
-    //             ${this.comps.Render("calculator")}
-    //         </div>
-    //     `;
     return `
-    <div>
-        lobby
-    </div>
+          <div class="center-div host-page-container">
+            <img src="/public/assets/brutalism/Brutalist 79.png" alt="random illustration" />
+            <div class="join-prompt vertical">
+              <h1>Waiting For Host...</h1>
+              <h2>${window.location.href.split("/").pop()}</h2>
+            </div>
+              <div class="lobby-player-list-container">
+                      ${session.state.players?.map((player, i) => {
+                        return `
+                            <div class="lobby-player-list-item ${
+                              player.ready ? "ready" : ""
+                            }">
+                              <div class="lobby-player-list-item-vertical">
+                                <div class="lobby-player-list-item-username">
+                                  ${player.username}
+                                </div>
+                                <div class="lobby-player-list-item-ready">
+                                  ${player.ready ? "Ready" : "Not Ready"}
+                                </div>
+                              </div>
+                          </div>
+
+                          `;
+                      })}
+              </div>
+              <div class="right-aligned-button-group">
+                  ${
+                    session.state.players?.find(
+                      (player) => player.socketId === session.socket.id
+                    )?.ready
+                      ? ""
+                      : `
+                          <button id="readyButton">Ready Up</button>
+                        `
+                  }  
+                  <button class="button-danger" onclick={}>Exit</button>
+              </div>
+          </div>
     `;
   }
 }
